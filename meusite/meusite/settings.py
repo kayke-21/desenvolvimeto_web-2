209655a40c9aea457f960a29e089d7cf069a672e
kayke-21 <kaykequaresma78@gmail.com>
2026-05-21 21:30:44 -0400

    aula 4  completa

--- meusite/meusite/settings.py
+++ meusite/meusite/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILE_DIRS = [BASE_DIR/ 'static']
